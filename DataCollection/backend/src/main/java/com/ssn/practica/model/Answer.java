a371582be105c5cc60d6587ee0b0d5cfe4023b57
alexcaltun <alex.alex.caltun20@gmail.com>
2024-08-18 15:33:04 +0300

    answerId added + toString and equals by answerId

--- DataCollection/backend/src/main/java/com/ssn/practica/model/Answer.java
+++ DataCollection/backend/src/main/java/com/ssn/practica/model/Answer.java
@@ -1,6 +1,7 @@
 package com.ssn.practica.model;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +15,9 @@ public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
+
+	@JoinColumn(nullable = false)
+	private String answerId;
 
 	@JoinColumn(nullable = false)
 	private String answer;
@@ -59,6 +63,40 @@ public class Answer {
 
 	public void setTask(Task task) {
 		this.task = task;
+	}
+
+	public String getAnswerId() {
+		return answerId;
+	}
+
+	public void setAnswerId(String answerId) {
+		this.answerId = answerId;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", answerId=" + answerId + ", answer=" + answer + ", date=" + date + "taskId= "
+				+ task.getTaskId() + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(answerId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Answer other = (Answer) obj;
+		return Objects.equals(answerId, other.answerId);
 	}
 
 }
